5bf831279dd204049f74d8dba60e908fc25a853b
j-beresford <68725342+j-beresford@users.noreply.github.com>
2022-08-18 15:59:10 +0100

    aws

--- call-data.R
+++ call-data.R
@@ -4,3 +4,9 @@ url<-"https://kf.kobotoolbox.org/api/v2/assets/aJ5NwkApvziLAUE7i9eHcn/data.json"
 rawdata<-GET(url,authenticate(u,pw),progress())
 p<-jsonlite::parse_json(rawdata)
 results<-p$results
+
+maps<-data.frame(a=c(1,2,3))
+
+saveRDS(maps,file="data_backup/maps.Rdata")
+put_object(file = "data_backup/maps.Rdata",
+           object = "maps.Rdata",bucket = s3BucketName)
