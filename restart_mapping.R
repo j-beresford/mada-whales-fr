21649a7593fc20cbedfbaaa3f93140a3aa0a07db
j-beresford <68725342+j-beresford@users.noreply.github.com>
2022-09-25 08:32:23 +0100

    mapping

--- restart_mapping.R
+++ restart_mapping.R
@@ -5,9 +5,7 @@ mapping_old<-s3readRDS(object = "map.rds", bucket = "mada-whales")
 
 #Make changes
 map<-mapping_old%>%
-  filter(!is.na(sighting_id))%>%
-  filter(i3s_id!="MD-432.jpg")%>%
-  filter(!sighting_id %in% c("aa9d4349-c62a","066a7396-b9fd","cc9bd689-f519","3c38ad36-e2df"))
+  filter(!sighting_id %in% c("6952234d-dd3c"))
 
 
 
